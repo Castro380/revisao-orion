6d6f05ca0b0163f0a9e6802a43c192519f9296cf
Castro380 <mateuso@iesb.edu.br>
2023-05-06 13:31:39 -0300

    Revisão-Orion

---
+++ pages/deputados/[id].js
@@ -0,0 +1,83 @@
+
+import { Card, Col, Row, Table } from 'react-bootstrap'
+import Pagina from '@/componentes/Pagina'
+import apiDeputados from '@/services/apiDeputados'
+
+
+const Detalhes = ({ deputado, deputado_despesas, deputado_profissoes }) => {
+  return (
+    <Pagina titulo={deputado.nomeCivil}>
+        <Row md={3}>
+       <Card>
+              <Card.Img src={deputado.ultimoStatus.urlFoto} />
+              <Card.Body>
+                <Card.Title>{deputado.ultimoStatus.nomeCivil}</Card.Title>
+                  <p>Partido: {deputado.ultimoStatus.siglaPartido}</p>
+                  <p>UF: {deputado.ultimoStatus.siglaUf}</p> 
+              </Card.Body>
+            </Card>
+        
+
+    <Col>
+      <Table striped>
+      <thead>
+        <tr>
+          <th>Data</th>
+          <th>Descrição</th>
+          <th>Valor</th>
+        </tr>
+      </thead>
+      <tbody>
+      {deputado_despesas.map(item => (
+         <tr>
+         <td>{item.dataDocumento}</td>
+         <td>{item.tipoDespesa}</td>
+         <td> R${item.valorDocumento}</td>
+       </tr>
+        ))}
+      </tbody>
+    </Table>
+    </Col>
+
+    <Col md={2}>
+
+<h2>Profissões</h2>        
+
+<ul>
+    {deputado_profissoes.map(item => (
+      <li>{item.titulo}</li>
+    ))}
+</ul>
+
+</Col>
+
+    </Row>
+    </Pagina>
+  )
+}
+
+export default Detalhes
+
+
+export async function getServerSideProps(context) {
+
+  const id = context.params.id
+
+  const resultado = await apiDeputados.get("/deputados/" + id)
+  const deputado = resultado.data.dados
+
+  const despesas = await apiDeputados.get("/deputados/" + id + "/despesas")
+  const deputado_despesas = despesas.data.dados
+
+  const profissoes = await apiDeputados.get("/deputados/" + id + "/profissoes")
+  const deputado_profissoes = profissoes.data.dados
+
+  return {
+    props: {
+      deputado,
+      deputado_despesas,
+      deputado_profissoes
+    
+    }, // will be passed to the page component as props
+  }
+}
---
+++ pages/deputados/index.js
@@ -0,0 +1,38 @@
+import Pagina from '@/componentes/Pagina'
+import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { Col, Row } from 'react-bootstrap'
+import Link from 'next/link'
+import apiDeputados from '@/services/apiDeputados'
+
+const index = ({deputados}) => {
+    
+
+    return (
+        <Pagina titulo='Deputados'>
+            <h2>Deputados</h2>
+            <Row>
+                {deputados.map(item => (
+                    <Col className='mb-3' md={2} title={item.foto}>
+                        <Link href={'/deputados/' + item.id}>
+                            <img key={item.id} src={item.urlFoto} width="100px" />
+                        </Link>
+                    </Col>
+                ))}
+            </Row>
+        </Pagina>
+    )
+}
+
+export default index
+
+export async function getServerSideProps(context) {
+    const resultado = await apiDeputados.get("/deputados")
+    const deputados = resultado.data.dados
+  
+    return {
+      props: {
+        deputados
+      }, // will be passed to the page component as props
+    }
+  }
